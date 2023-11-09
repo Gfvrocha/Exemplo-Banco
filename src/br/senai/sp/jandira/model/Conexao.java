8a1758c759c36e8b83e7355e4eee7a830da75a90
VITOR SANTANA DE JESUS <vitor.alunos@cfp127aluno.local>
2023-11-09 16:45:59 -0300

    Exemplo Conexão Banco

---
+++ src/br/senai/sp/jandira/model/Conexao.java
@@ -0,0 +1,43 @@
+package br.senai.sp.jandira.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+
+    private String banco, password, servidor, user;
+
+    public Connection conexao;
+
+    public Conexao (){
+        this.servidor = "localhost";
+        this.banco = "db_java_senai";
+        this.user = "root";
+        this.password = "bcd127";
+
+
+    }
+
+    public boolean connectDrive(){
+        try {
+
+            this.conexao = DriverManager.getConnection("jdbc:mysql://" + this.servidor + "/" + this.banco, this.user, this.password);
+
+            return true;
+
+
+
+        }catch (Exception erro){
+            System.out.println(erro);
+
+            return false;
+        }
+    }
+
+    public Connection getConnection(){
+        connectDrive();
+        return  conexao;
+    }
+
+
+}
